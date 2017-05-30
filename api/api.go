e662b46b129c2a669211400e348d96db60c89447
avi <hi@avi.im>
2017-05-30 21:41:33 +0530

    Add error handling

--- api/api.go
+++ api/api.go
@@ -4,6 +4,7 @@ import "fmt"
 
 type XKCD struct {
 	DownloadDir string
+	SaveImage   bool
 }
 
 type Comic struct {
@@ -22,32 +23,52 @@ type Comic struct {
 
 var baseUrl = "https://www.xkcd.com/%d/info.0.json"
 
-func (x *XKCD) GetLatest() Comic {
+func (x *XKCD) GetLatest() (Comic, error) {
+	var err error
 	url := "https://www.xkcd.com/info.0.json"
 	comic := Comic{}
-	getJson(url, &comic)
-	return comic
+	err = getJson(url, &comic)
+	if err == nil && x.SaveImage {
+		err = x.getImage(comic)
+	}
+	return comic, err
 }
 
-func (x *XKCD) Get(comicNum int) Comic {
+func (x *XKCD) Get(comicNum int) (Comic, error) {
+	var err error
 	url := fmt.Sprintf(baseUrl, comicNum)
 	comic := Comic{}
-	getJson(url, &comic)
-	return comic
+	err = getJson(url, &comic)
+	if err == nil && x.SaveImage {
+		err = x.getImage(comic)
+	}
+	return comic, err
 }
 
-func (x *XKCD) GetAll() []Comic {
-	latest := x.GetLatest().Num
-	latest = 5
-	return x.GetRange(1, latest)
+func (x *XKCD) GetAll() ([]Comic, error) {
+	comic, err := x.GetLatest()
+	if err != nil {
+		return nil, err
+	}
+	return x.GetRange(1, comic.Num)
 }
 
-func (x *XKCD) GetRange(start, end int) []Comic {
+func (x *XKCD) GetRange(start, end int) ([]Comic, error) {
 	comics := []Comic{}
 	for _, comicNum := range makeRange(start, end) {
-		comics = append(comics, Get(comicNum))
+		comic, err := x.Get(comicNum)
+		if err != nil {
+			return comics, err
+		}
+		comics = append(comics, comic)
 	}
-	return comics
+	return comics, nil
+}
+
+func (x *XKCD) getImage(comic Comic) error {
+	filePath := fmt.Sprintf("%s/%d - %s.png",
+		x.DownloadDir, comic.Num, comic.Title)
+	return saveImage(comic.Img, filePath)
 }
 
 func makeRange(min, max int) []int {
