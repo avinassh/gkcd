2e161b6084a63ef747e6f14fcd2d5e3a2e5b3008
avi <hi@avi.im>
2017-05-31 21:04:12 +0530

    Update exmaple

--- main.go
+++ main.go
@@ -1,5 +1,21 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"os"
 
+	xkcd "github.com/avinassh/gkcd/api"
+)
+
+func main() {
+	DownloadDir := "/Users/avi/xkcd"
+	err := os.MkdirAll(DownloadDir, os.ModePerm)
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+	comic, err := xkcd.GetLatest()
+	if err != nil {
+		fmt.Println(err)
+	}
+	xkcd.SaveComicWithMeta(comic, DownloadDir)
 }
